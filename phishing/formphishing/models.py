9589c2a32fb6c654fdf1e94f25ed43cada4d6e61
Nuevo nombre de usuario <nuevo@email.com>
2023-08-21 20:06:17 -0500

    testph2

--- phishing/formphishing/models.py
+++ phishing/formphishing/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 # models.py
 
-from django.db import models
 
 class Usuario(models.Model):
     correo_corporativo = models.EmailField()
     clave = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.correo_corporativo
+
